a1f0f842f5800d7dc37f613aac6a4ac96adcef90
Abhishek Gupta <abhi.bansal21@gmail.com>
2013-10-14 17:11:53 +0530

    Approach II:Getting Time Limit exceeded
    Decrease the length of the substring starting from the complete word.
    
    If its in dict then call same function on the remaining
    string.
    	If it returns true then return true else decrease the
    	length of the substring

--- Solution.java
+++ Solution.java
@@ -1,10 +1,10 @@
 public class Solution {
     public boolean wordBreak(String s, Set<String> dict) {
         // Note: The Solution object is instantiated only once and is reused by each test case.
-        if(s == ""){
+        if(s.length() == 0){
             return true;
         }
-        for (int i=1; i<=s.length(); i++){
+        for (int i=s.length(); i>0; i--){
             if(dict.contains(s.substring(0,i))){
                 if(wordBreak(s.substring(i), dict)){
                     return true;
--- test.java
+++ test.java
@@ -4,8 +4,8 @@ public class test {
     
     public static void main(String[] args){
         test a = new test();
-        // System.out.println(a.wordBreak("acaaaaabbbdbcccdcdaadcdccacbcccabbbbcdaaaaaadb", new HashSet<String>(Arrays.asList("abbcbda","cbdaaa","b","dadaaad","dccbbbc","dccadd","ccbdbc","bbca","bacbcdd","a","bacb","cbc","adc","c","cbdbcad","cdbab","db","abbcdbd","bcb","bbdab","aa","bcadb","bacbcb","ca","dbdabdb","ccd","acbb","bdc","acbccd","d","cccdcda","dcbd","cbccacd","ac","cca","aaddc","dccac","ccdc","bbbbcda","ba","adbcadb","dca","abd","bdbb","ddadbad","badb","ab","aaaaa","acba","abbb"))));
-        System.out.println(a.wordBreak("leetcode", new HashSet<String>(Arrays.asList("leet", "code"))));
+        System.out.println(a.wordBreak("acaaaaabbbdbcccdcdaadcdccacbcccabbbbcdaaaaaadb", new HashSet<String>(Arrays.asList("abbcbda","cbdaaa","b","dadaaad","dccbbbc","dccadd","ccbdbc","bbca","bacbcdd","a","bacb","cbc","adc","c","cbdbcad","cdbab","db","abbcdbd","bcb","bbdab","aa","bcadb","bacbcb","ca","dbdabdb","ccd","acbb","bdc","acbccd","d","cccdcda","dcbd","cbccacd","ac","cca","aaddc","dccac","ccdc","bbbbcda","ba","adbcadb","dca","abd","bdbb","ddadbad","badb","ab","aaaaa","acba","abbb"))));
+        // System.out.println(a.wordBreak("leetcode", new HashSet<String>(Arrays.asList("leet", "code"))));
     }
 
     public boolean wordBreak(String s, Set<String> dict) {
@@ -13,7 +13,7 @@ public class test {
         if(s.length() == 0){
             return true;
         }
-        for (int i=1; i<=s.length(); i++){
+        for (int i=s.length(); i>0; i--){
             if(dict.contains(s.substring(0,i))){
                 if(wordBreak(s.substring(i), dict)){
                     return true;
